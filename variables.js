43d083ef39f3a13e7d56a7e4ce8985820b2e2a43
Anuj Kumar <anujkumar75459@gmail.com>
2026-07-28 20:56:57 +0530

    added variables.js

---
+++ variables.js
@@ -0,0 +1,20 @@
+const userId=892
+var userName="anuj kr"
+let userCourse="btech"
+let userEmail;//undecleared variables are undefined
+
+
+//userId=893     can't assign any const variable
+var userName="jinn"   
+userCourse="cse"
+
+console.log(userId)
+console.log(userName)
+console.log(userCourse)
+
+console.table([userId,userName,userCourse])
+
+/*
+in let variable redeclaration in not allowed while
+in var we can redeclear so that we don't we var as a variable
+*/
